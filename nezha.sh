e2c9e31eaf510974db27347ab034c05048298893
rea-tool <105956707+rea-tool@users.noreply.github.com>
2022-07-11 23:53:46 +0800

    Update nezha.sh

--- nezha.sh
+++ nezha.sh
@@ -5,4 +5,4 @@ NZ_AGENT_PATH="${NZ_BASE_PATH}/agent"
 
 chmod +x $NZ_AGENT_PATH/nezha-agent
 
-./opt/nezha/agent/nezha-agent -s nz_grpc_host:nz_grpc_port -p nz_client_secret
+/opt/nezha/agent/nezha-agent -s nz_grpc_host:nz_grpc_port -p nz_client_secret &
